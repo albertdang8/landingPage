c5a7384600dd06d4a50a6847b2cceb63c5e43544
Albert <albertdang8@gmail.com>
2023-08-16 14:21:39 -0700

    nav 'completed'

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import manageLogo from '../../assets/images/logo.svg'
+
 import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='nav-container'>
+      <div className="logo">
+        <img src={manageLogo} className='logo' alt='Manage logo'/>
+      </div>
+      <div className="nav">
+        <a href='#' className='nav-btn'>Product</a>
+        <a href='#' className='nav-btn'>Pricing</a>
+        <a href='#' className='nav-btn'>About Us</a>
+        <a href='#' className='nav-btn'>Careers</a>
+        <a href='#' className='nav-btn'>Community</a>
+      </div>
+      <div className="start-btn-nav">
+        <a href="#" className='start-btn'>Get Started</a>
+      </div>
+      
+    </div>
   )
 }
 
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import Banner from "../../components/Banner/Banner";
+import Features from "../../components/Features/Features";
+import Testimonials from "../../components/Testimonials/Testimonials";
+import CallToAction from "../../components/CallToAction/CallToAction";
+import Footer from "../../components/Footer/Footer";
+
 import "./Homepage.css";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <Navbar />
+      <Banner />
+      <Features />
+      <Testimonials />
+      <CallToAction />
+      <Footer />
+    </div>
+  );
 };
 
 export default Homepage;
