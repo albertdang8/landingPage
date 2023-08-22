e5075344d4245b2ca26860b6075a79f431f966fc
Albert Dang <98789245+albertdang8@users.noreply.github.com>
2023-08-21 18:30:17 -0700

    slider looks like monster poop rm

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './App.css'
-import Homepage from './pages/Homepage/Homepage'
+import Navbar from "./components/Navbar/Navbar";
+import Homepage from "./pages/Homepage/Homepage";
+import CallToAction from "./components/CallToAction/CallToAction";
+import Footer from "./components/Footer/Footer";
+
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-      <Homepage />
-    </>
-  )
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+
+      <CallToAction />
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,23 +1,17 @@
 import React from "react";
 
-import Navbar from "../../components/Navbar/Navbar";
 import Banner from "../../components/Banner/Banner";
 import Features from "../../components/Features/Features";
 import Testimonials from "../../components/Testimonials/Testimonials";
-import CallToAction from "../../components/CallToAction/CallToAction";
-import Footer from "../../components/Footer/Footer";
 
 import "./Homepage.css";
 
 const Homepage = () => {
   return (
     <div>
-      <Navbar />
       <Banner />
       <Features />
       <Testimonials />
-      <CallToAction />
-      <Footer />
     </div>
   );
 };
