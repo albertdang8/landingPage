f30e597edaf0d4c2693f0e7c791fc0ada7fbb49b
Albert Dang <98789245+albertdang8@users.noreply.github.com>
2023-08-29 14:38:14 -0700

    roots

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -18,9 +18,12 @@ const Testimonials = () => {
     slidesToShow: 2,
     slidesToScroll: 2,
   };
+  
   return (
     <div className="testSlider">
-      <h2 className="boop2">What they've said</h2>
+      <div className="head-title">
+        <h2 className="boop2">What they've said</h2>
+      </div>
 
       <Slider {...settings} className="testimonial-container">
         <div>
@@ -93,60 +96,3 @@ const Testimonials = () => {
 };
 
 export default Testimonials;
-
-{
-  /* <div className="testimony-slider boop2">
-  <div className="testimony-card">
-    <img
-      src={anishaImg}
-      className="testimonyImg"
-      alt="Ali Bravo's image"
-    />
-    <h3>Anisha Li</h3>
-    <p className="testimony">
-      "We have been able to cancel so many other subscriptions since
-      using Manage. There is no more cross-channel confusion and
-      everyone is much more focused."
-    </p>
-  </div>
-  <div className="testimony-card">
-    <img
-      src={aliImg}
-      className="testimonyImg"
-      alt="Ali Bravo's image"
-    />
-    <h3>Ali Bravo</h3>
-    <p className="testimony">
-      "Manage has supercharged our team's workflow. The ability to
-      maintain visibility on larger milestones at all times keeps
-      everyone motivated."
-    </p>
-  </div>
-  <div className="testimony-card">
-    <img
-      src={richardImg}
-      className="testimonyImg"
-      alt="Ali Bravo's image"
-    />
-    <h3>Richard Watts</h3>
-    <p className="testimony">
-      "Manage allows us to provide structure and process. It keeps us
-      organized and focused. I can't stop recommending them to everyone
-      I talk to!"
-    </p>
-  </div>
-  <div className="testimony-card">
-    <img
-      src={shanaiImg}
-      className="testimonyImg"
-      alt="Ali Bravo's image"
-    />
-    <h3>Shanai Gough</h3>
-    <p className="testimony">
-      "Their software allows us to track, manage and collaborate on our
-      projects from anywhere. It keeps the whole team in sync without
-      being intrusive."
-    </p>
-  </div>
-</div> */
-}
